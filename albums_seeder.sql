08dc2f1eb8627298824c60318997d5005f9d2905
Tianrun Zhang <zhang_tianrun@yahoo.com>
2023-01-20 12:05:47 -0600

    statement exercise

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+INSERT INTO albums (Artist,Album,Released, Genre, CLaimed_sales_mil)
+VALUES ('Michael Jackson','Thriller','1982','Pop','51.2'),
+       ('AC/DC','Back in Black','1980','Hardrock','30.1');
+
+INSERT INTO albums (Artist,Album,Released, Genre, CLaimed_sales_mil)
+VALUES ('Whitney Houston','The bodyguard','1992','R&B','28.7'),
+       ('Pink Floyd','The Dark Side of the Moon','1973','Progressive rock','24.8');

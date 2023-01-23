40c79c110124c6f1636b721e4a7caa50db27ce36
Tianrun Zhang <zhang_tianrun@yahoo.com>
2023-01-23 09:03:40 -0600

    sql exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,9 @@
 USE codeup_test_db;
-INSERT INTO albums (Artist,Album,Released, Genre, CLaimed_sales_mil)
-VALUES ('Michael Jackson','Thriller','1982','Pop','51.2'),
-       ('AC/DC','Back in Black','1980','Hardrock','30.1');
+INSERT INTO albums (artist_name,album_name,release_date, sales,genre)
+VALUES ('Michael Jackson','Thriller',1982,51.2,'pop'),
+       ('AC/DC','Back in Black',1980,30.1,'hardrock');
 
-INSERT INTO albums (Artist,Album,Released, Genre, CLaimed_sales_mil)
-VALUES ('Whitney Houston','The bodyguard','1992','R&B','28.7'),
-       ('Pink Floyd','The Dark Side of the Moon','1973','Progressive rock','24.8');
+INSERT INTO albums (artist_name,album_name,release_date, sales,genre)
+VALUES ('Whitney Houston','The bodyguard',1992,28.7,'r&b'),
+       ('Pink Floyd','The Dark Side of the Moon',1973,24.8,'progressive rock');
+-- mysql -u codeup_test_user -p < albums_seeder.sql
